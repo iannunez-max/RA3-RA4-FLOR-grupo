e2923328db1b27e52e88115f2fa3355eb5fa1c61
CocaAleix <aleixgoungra@alumnes.ilerna.com>
2025-01-31 17:01:49 +0100

    Part 1

---
+++ RA3-RA4/Main.java
@@ -0,0 +1,35 @@
+package SimpleBankingSystem;
+/**
+*
+* @author Flor Martinez
+*/
+public class Main {
+	 public static void main(String[] args) {
+		Account myAccount;
+	        
+	    myAccount = new Account("Flor Martinez", "1000-1234-56-123456789", 2500);
+	     
+	    myAccount.withdrawAmount(2300); // Breakpoint aquí
+
+	    myAccount.depositAmount(1695); // Breakpoint aquí
+
+	    System.out.println("El saldo actual és " + myAccount.getBalance());
+	    
+	    try {
+	    	myAccount.withdrawAmount(2300);
+	    } catch(Exception e){
+        	System.err.println(e.getMessage());
+            System.out.println("Error al retirar");
+	    }
+	     
+	    try {
+	    	 System.out.println("Ingrés al compte");
+	    	 myAccount.depositAmount(1695);
+	    } catch(Exception e){
+	    	 System.err.println(e.getMessage());
+	         System.out.println("Error en l'ingrés");
+	    }
+
+	     System.out.println("El saldo actual es " + myAccount.getBalance());   
+	 }
+}
